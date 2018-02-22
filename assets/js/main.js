cc415ebe63ede35484ff7191ffda80a1ef5a6217
mujammal <34380345+mujammal@users.noreply.github.com>
2018-02-23 05:17:16 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,145 +8,126 @@ function myFunction(x) {
 }
 
 
+
 /* ==================================================
  * Cuda main js
  * ==================================================
  */
 
-(function ($) {
+$(document).ready(function(){ // Make DOM and other elements ready
     
-    /* Preloader
-     * -------------------------------------------------- */
-    $WIN = $(window);
-    var ssPreloader = function () {
-        $("html").addClass('ss-preload');
-        $WIN.on('load', function () {
-            // force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-            // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function () {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            });
-            // for hero content animations 
-            $("html").removeClass('ss-preload');
-            $("html").addClass('ss-loaded');
+/* ==================================================
+ * Preloader
+ * ==================================================
+ */
+var ssPreloader = function () {
+    $("html").addClass('ss-preload');
+    $(window).on('load', function () {
+        // force page scroll position to top at page refresh
+        // $('html, body').animate({ scrollTop: 0 }, 'normal');
+        // will first fade out the loading animation 
+        $("#loader").fadeOut("slow", function () {
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
         });
-    };
-    /* Initialize
-     * ------------------------------------------------------ */
-    (function ssInit() {
-        ssPreloader();
-    })();
-
-    /* Smooth Scroll
-     * -------------------------------------------------- */
-    $(document).ready(function () {
-        // Add smooth scrolling to all links
-        $("a").on('click', function (event) {
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
-                // Store hash
-                var hash = this.hash;
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function () {
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            } // End if
-        });
-    });   
-
-})(jQuery);  //main bracket
-
-
+        // for hero content animations 
+        $("html").removeClass('ss-preload');
+        $("html").addClass('ss-loaded');
+    });
+};
+ssPreloader();
+    
+ /* ==================================================
+  * Smooth Scroll
+  * ==================================================
+  */
+ // Add smooth scrolling to all links
+ $("a").on('click', function (event) {
+     // Make sure this.hash has a value before overriding default behavior
+     if (this.hash !== "") {
+         // Prevent default anchor click behavior
+         event.preventDefault();
+         // Store hash
+         var hash = this.hash;
+         // Using jQuery's animate() method to add smooth page scroll
+         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+         $('html, body').animate({
+             scrollTop: $(hash).offset().top
+         }, 800, function () {
+             // Add hash (#) to URL when done scrolling (default click behavior)
+             window.location.hash = hash;
+         });
+     } // End if
+ });
+    
 /* ==================================================
  * More Button
  * ==================================================
  */
+$("#loadMore").on("click", function(event) {
+  event.preventDefault();
+  var category = $("button.active")[0].id;
+  var $hiddenProjects = $(".project.hidden");
+  if (category !== "all") {
+    $hiddenProjects = $hiddenProjects.filter(function() {
+    return $(this).data("cat") === category ;
+  });
+  }
+  $hiddenProjects.slice(0,2).each(function() {
+    $(this).hide().removeClass('hidden not-filter').show(500);
+  });
+});    
+    
+ /* ==================================================
+  * Filtering Buttons
+  * ==================================================
+  */
+ function filterPortfolio(event) {
+     $("button.active").removeClass("active");
+     $(this).addClass("active");
+     var targetId = event.target.id;
+     $(".work .project").not(".not-filter").each(function () {
+         var $this = $(this);
+         $this.addClass("hidden");
+         if (targetId === $this.data("cat") || targetId === "all") {
+             $this.removeClass("hidden");
+         }
+     });
+ }
+ $(".filter-buttons").on("click", "button", filterPortfolio);
+    
+ /* ==================================================
+  * Pie Charts
+  * ==================================================
+  */
+ $(".pie-chart").each(function () {
+     var $pieChart = $(this);
+     var percent = $pieChart.attr('data-percent');
+     var $path = $pieChart.find('path');
+     var pathLength = $path[0].getTotalLength();
+     var offsetTo = pathLength * (percent / 100);
 
-$("#loadMore").on("click", function (event) {
-    event.preventDefault();
-    var category = $("button.active")[0].id;
-    var $hiddenProjects = $(".project.hidden");
-    if (category !== "all") {
-        $hiddenProjects = $hiddenProjects.filter(function () {
-            return $(this).data("cat") === category;
-        });
-    }
-    $hiddenProjects.slice(0, 2).each(function () {
-        $(this).hide().removeClass('hidden not-filter').show(500);
-    });
-});
+     $pieChart.attr('data-percent', 0);
+     $path[0].style.strokeDashoffset = 0;
 
-/* ==================================================
- * Filtering Buttons
- * ==================================================
- */
+     $pieChart.one("inview", function (event) {
+         $path.animate({
+             'strokeDashoffset': offsetTo
+         }, {
+             duration: 2500,
+             step: function (now) {
+                 $pieChart.attr('data-percent', Math.round(now / pathLength * 100));
+             }
+         });
+     });
+ });  
+    
 
-function filterPortfolio(event) {
-    $("button.active").removeClass("active");
-    $(this).addClass("active");
-    var targetId = event.target.id;
-    $(".work .project").not(".not-filter").each(function () {
-        var $this = $(this);
-        $this.addClass("hidden");
-        if (targetId === $this.data("cat") || targetId === "all") {
-            $this.removeClass("hidden");
-        }
-    });
-}
-$(".filter-buttons").on("click", "button", filterPortfolio);
-
-/* ==================================================
- * Pie Charts
- * ==================================================
- */
-
-$(".pie-chart").each(function () {
-    var $pieChart = $(this);
-    var percent = $pieChart.attr('data-percent');
-    var $path = $pieChart.find('path');
-    var pathLength = $path[0].getTotalLength();
-    var offsetTo = pathLength * (percent / 100);
-
-    $pieChart.attr('data-percent', 0);
-    $path[0].style.strokeDashoffset = 0;
-
-    $pieChart.one("inview", function (event) {
-        $path.animate({
-            'strokeDashoffset': offsetTo
-        }, {
-            duration: 2500,
-            step: function (now) {
-                $pieChart.attr('data-percent', Math.round(now / pathLength * 100));
-            }
-        });
-    });
-});
-
-/* ==================================================
- * Nice Scroll
- * ==================================================
- */
-
-$("html").niceScroll({
-    cursorcolor: "#fff",
-    cursoropacitymax: 0.7,
-    boxzoom: true,
-    touchbehavior: true
-});
-
+    
 /* ==================================================
  * Wow Animation
  * ==================================================
  */
-
 wow = new WOW({
     animateClass: 'animated',
     offset: 100,
@@ -155,16 +136,11 @@ wow = new WOW({
     }
 });
 wow.init();
-document.getElementById('moar').onclick = function () {
-    var section = document.createElement('section');
-    section.className = 'section--purple wow fadeInDown';
-    this.parentNode.insertBefore(section, this);
-};
+  
 
-
-
-
-
+    
+    
+});  //ready function bracket 
 
 
 
