34bcb40c63384aa43282fe4c0cd39faa474dedda
mujammal <34380345+mujammal@users.noreply.github.com>
2018-02-22 18:02:00 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,74 @@
  * Toggle hamburger menu
  * ==================================================
  */
+
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+
+/* ==================================================
+ * Cuda main js
+ * ==================================================
+ */
+
+(function ($) {
+    
+    /* Preloader
+     * -------------------------------------------------- */
+    $WIN = $(window);
+    var ssPreloader = function () {
+        $("html").addClass('ss-preload');
+        $WIN.on('load', function () {
+            // force page scroll position to top at page refresh
+            // $('html, body').animate({ scrollTop: 0 }, 'normal');
+            // will first fade out the loading animation 
+            $("#loader").fadeOut("slow", function () {
+                // will fade out the whole DIV that covers the website.
+                $("#preloader").delay(300).fadeOut("slow");
+            });
+            // for hero content animations 
+            $("html").removeClass('ss-preload');
+            $("html").addClass('ss-loaded');
+        });
+    };
+    /* Initialize
+     * ------------------------------------------------------ */
+    (function ssInit() {
+        ssPreloader();
+    })();
+
+    /* Smooth Scroll
+     * -------------------------------------------------- */
+    $(document).ready(function () {
+        // Add smooth scrolling to all links
+        $("a").on('click', function (event) {
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+                // Store hash
+                var hash = this.hash;
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            } // End if
+        });
+    });   
+
+})(jQuery);  //main bracket
+
 
 /* ==================================================
  * More Button
  * ==================================================
  */
+
 $("#loadMore").on("click", function (event) {
     event.preventDefault();
     var category = $("button.active")[0].id;
@@ -28,6 +88,7 @@ $("#loadMore").on("click", function (event) {
  * Filtering Buttons
  * ==================================================
  */
+
 function filterPortfolio(event) {
     $("button.active").removeClass("active");
     $(this).addClass("active");
@@ -46,6 +107,7 @@ $(".filter-buttons").on("click", "button", filterPortfolio);
  * Pie Charts
  * ==================================================
  */
+
 $(".pie-chart").each(function () {
     var $pieChart = $(this);
     var percent = $pieChart.attr('data-percent');
@@ -72,17 +134,19 @@ $(".pie-chart").each(function () {
  * Nice Scroll
  * ==================================================
  */
+
 $("html").niceScroll({
     cursorcolor: "#fff",
     cursoropacitymax: 0.7,
     boxzoom: true,
     touchbehavior: true
 });
- 
+
 /* ==================================================
  * Wow Animation
  * ==================================================
  */
+
 wow = new WOW({
     animateClass: 'animated',
     offset: 100,
@@ -98,82 +162,17 @@ document.getElementById('moar').onclick = function () {
 };
 
 
-/* ==================================================
- * Cuda main js
- * ==================================================
- */
-
-(function ($) {
-    "use strict";
-    var cfg = {
-            scrollDuration: 800, // smoothscroll duration
-            mailChimpURL: '' // mailchimp url
-        },
-        $WIN = $(window);
-    // Add the User Agent to the <html>
-    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
-    var doc = document.documentElement;
-    doc.setAttribute('data-useragent', navigator.userAgent);
-
-    /* Preloader
-     * -------------------------------------------------- */
-    var ssPreloader = function () {
-
-        $("html").addClass('ss-preload');
-
-        $WIN.on('load', function () {
-
-            // force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function () {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            });
-
-            // for hero content animations 
-            $("html").removeClass('ss-preload');
-            $("html").addClass('ss-loaded');
-
-        });
-    };
 
 
-    /* Smooth Scroll
-     * -------------------------------------------------- */
-    $(document).ready(function () {
-        // Add smooth scrolling to all links
-        $("a").on('click', function (event) {
-
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
-
-                // Store hash
-                var hash = this.hash;
-
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function () {
-
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            } // End if
-        });
-    });
 
 
-    /* Initialize
-     * ------------------------------------------------------ */
-    (function ssInit() {
-        ssPreloader();
-    })();
 
-})(jQuery);
+
+
+
+
+
+
+
 
 
